1b4ff0ead9bf07f96445b283b361e74be6f9c218
Mhaurel <maxime.haurel0@gmail.com>
2023-04-17 19:13:58 +0200

    Changed colors and spacings

--- src/components/NavigationButton.js
+++ src/components/NavigationButton.js
@@ -3,7 +3,7 @@ import ButtonBase from '@mui/material/ButtonBase';
 
 function NavigationButton({label, icon, onClick, active}) {
     return (
-        <ButtonBase sx={{backgroundColor: active ? "#D9E7CB" : "transparent"}} onClick={onClick} className="navigationButton">
+        <ButtonBase sx={{backgroundColor: active ? "#70D9FF" : "transparent"}} onClick={onClick} className="navigationButton">
             <img src={process.env.PUBLIC_URL + `images/icons/${icon}.png`} alt="Navigation Icon"/>
             <p>{label}</p>
         </ButtonBase>
