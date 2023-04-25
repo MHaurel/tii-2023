829bbfe8b051f5a52f00cc2b981aa4c8735d64b5
Mhaurel <maxime.haurel0@gmail.com>
2023-04-25 13:50:39 +0200

    Fixed footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,13 @@
 import Paper from '@mui/material/Paper';
-import "./Footer.css"
+import "./Footer.css";
+import { Link } from 'react-router-dom';
 
 function Footer() {
   return (
     <Paper className='footer'>
         <div className='footerDiv'>
            <div>
-                <img className='websiteLogo' src={process.env.PUBLIC_URL +'/images/fitTrack.png'} alt='FitTrack Logo'/>
+                <img className='websiteLogo' src={process.env.PUBLIC_URL +'/images/FitTrack.png'} alt='FitTrack Logo'/>
                 <p>135 Santa's avenue</p>
                 <p>Lapland</p>
                 <p>01483 319 533</p>
@@ -14,19 +15,20 @@ function Footer() {
 
            <div className='socialMedia' >
                 <div className='brandsLogo'>
-                    <img className='logo' src={process.env.PUBLIC_URL + '/images/icons/facebookLogo.png'} alt='Facebook Logo'/>
-                    <img className='logo' src={process.env.PUBLIC_URL + '/images/icons/tweeterLogo.png'} alt='Tweeter Logo'/>
+                    <a href="#"><img className='logo' src={process.env.PUBLIC_URL + '/images/icons/facebookLogo.png'} alt='Facebook Logo'/></a>
+                    <a href="#"><img className='logo' src={process.env.PUBLIC_URL + '/images/icons/tweeterLogo.png'} alt='Tweeter Logo'/></a>
                 </div>
                 <p>&copy;Copyright. All rights reserved.</p>
            </div>
 
            <div className='siteMap'>
-                <a>Home</a>
-                <a>Activities</a>
-                <a>Profile</a>
+               
+                <a href='/'>Home</a>
+                <a href='activities'>Activities</a>
+                {/* HREF to be determined */}
+                <a>Profile</a> 
            </div>
         </div>
-        
     </Paper>
   );
 }
