92703216a525a19dafd5f485c8bacfabb32390bb
JulioJamon54 <48096207+JulioJamon54@users.noreply.github.com>
2023-05-02 17:27:06 +0200

    login using mui components

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,30 @@
-//import React, { useEffect, useRef, useState } from "react";
+import React from 'react';
+import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
+import Input from '@mui/material/Input';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+import FormControl from '@mui/material/FormControl';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import TextField from '@mui/material/TextField';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Button from '@mui/material/Button';
+import Avatar from '@mui/material/Avatar';
 
 import "./Login.css";
 
 function Login() {
+    const [showPassword, setShowPassword] = React.useState(false);
+
+    const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+    const handleMouseDownPassword = (event) => {
+        event.preventDefault();
+    };
+
     return (
         <div className="login-page">
             <div className="login-container">
@@ -12,30 +34,56 @@ function Login() {
 
                 <h2 className="login-title"> Log In </h2>
 
-                <form className="login-form">
-                    <input type="text" id="email-phone" name="email-phone" placeholder="email or phone number" required />
-                    <input type="password" id="password" name="password" placeholder="password" required />
-
-                    <div className="remember-me">
-                        <input type="checkbox" id="remember-me" name="remember-me" />
-                        <label htmlFor="remember-me"> Remember me </label>
+                <Box component="form" sx={{"& > :not(style)": { m: 1 }}} noValidate autoComplete="off">
+                    <div className='login-box'>
+                        <TextField id="email-phone" label="Email or Phone Number" variant="standard" />
+                        <FormControl variant="standard" >
+                            <InputLabel htmlFor="standard-adornment-password">Password</InputLabel>
+                            <Input
+                                id="standard-adornment-password"
+                                type={showPassword ? 'text' : 'password'}
+                                endAdornment={
+                                    <InputAdornment position="end">
+                                        <IconButton
+                                            aria-label="toggle password visibility"
+                                            onClick={handleClickShowPassword}
+                                            onMouseDown={handleMouseDownPassword}
+                                        >
+                                            {showPassword ? <VisibilityOff /> : <Visibility />}
+                                        </IconButton>
+                                    </InputAdornment>
+                                }
+                            />
+                        </FormControl>
                     </div>
 
-                    <a href="#" className="forgot-password"> Forgot password? </a>
-                    <button type="submit" className="login-button"> Log In </button>
-
-                    <div className="social-login">
-                        <button className="google-button"> <img src={process.env.PUBLIC_URL + '/images/icons/googleLogo.png'} alt="google icon" /> Log in with Google </button>
-                        <button className="facebook-button"> <img src={process.env.PUBLIC_URL + '/images/icons/facebookLogo.png'} alt="facebook icon" /> Log in with Facebook </button>
+                    <div className='login-box'>
+                        <FormControlLabel control={<Checkbox />} label="Remember me" />
+                        <Link href="#" underline="always" color="inherit" sx={[{ '&:hover': { fontWeight: 'bold' } }]}> Forgot password? </Link>
                     </div>
-                </form>
+
+                    <div className='login-box'>
+                        <Button variant="contained">Log In</Button>
+                    </div>
+
+                    ------------------------------------------------------ or ------------------------------------------------------
+
+                    <div className='login-box'>
+                        <Button variant="outlined" startIcon={<Avatar src={process.env.PUBLIC_URL + '/images/icons/googleLogo.png'} alt="google icon" />}>
+                            Log in with Google
+                        </Button>
+                        <Button variant="outlined" startIcon={<Avatar src={process.env.PUBLIC_URL + '/images/icons/facebookLogo.png'} alt="facebook icon" />}>
+                            Log in with Facebook
+                        </Button>
+                    </div>
+                </Box>
             </div>
 
             <div className="login-illustration">
                 <img src={process.env.PUBLIC_URL + '/images/weightIllustration1.png'} alt="weight illustration" />
             </div>
         </div>
-    )
+    );
 }
 
 export default Login;
